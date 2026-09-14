691d5a475e6d32a870c7f58360c4a3c460d0f208
cryptikevans <evanzcryptik@gmail.com>
2026-09-14 20:11:37 +0300

    Remove trailing slash from SUPABASE_URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SELEKTA_CONFIG={
-  SUPABASE_URL:'https://sijyzgkjtpfxrgbybrgj.supabase.co/',
+  SUPABASE_URL:'https://sijyzgkjtpfxrgbybrgj.supabase.co',
   SUPABASE_PUBLISHABLE_KEY:'sb_publishable_pfUBHVpxjmCzFZkAW5Na5g_qfdNkrij'
 };
