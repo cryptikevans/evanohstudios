28a7cdbdcff6b2c1f098617262166641d74584f6
cryptikevans <evanzcryptik@gmail.com>
2026-09-14 19:46:16 +0300

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SELEKTA_CONFIG={
-  SUPABASE_URL:'https://YOUR-PROJECT-REF.supabase.co',
+  SUPABASE_URL:'https://sijyzgkjtpfxrgbybrgj.supabase.co/rest/v1/',
   SUPABASE_PUBLISHABLE_KEY:'YOUR_SUPABASE_PUBLISHABLE_KEY'
 };
