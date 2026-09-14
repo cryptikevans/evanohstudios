9a91b7ce4de392a67e1ce18176e13436022138c7
cryptikevans <evanzcryptik@gmail.com>
2026-09-14 19:52:12 +0300

    Update Supabase publishable key in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SELEKTA_CONFIG={
   SUPABASE_URL:'https://sijyzgkjtpfxrgbybrgj.supabase.co/rest/v1/',
-  SUPABASE_PUBLISHABLE_KEY:'YOUR_SUPABASE_PUBLISHABLE_KEY'
+  SUPABASE_PUBLISHABLE_KEY:'sb_publishable_pfUBHVpxjmCzFZkAW5Na5g_qfdNkrij'
 };
